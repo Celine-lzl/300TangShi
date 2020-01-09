3dcdf8c7a81d26f71073a391b617bee9af953dbe
Celine-lzl <1261271622@qq.com>
2020-01-09 20:04:51 +0800

    唐诗三百首

---
+++ tangshi/src/main/java/com/lzl/tangshianalyse/TangShiAnalyzeApplication.java
@@ -0,0 +1,10 @@
+package com.lzl.tangshianalyse;
+
+/**
+ * 唐诗三百首程序主类
+ */
+public class TangShiAnalyzeApplication {
+    public static void main(String[] args) {
+        System.out.println("唐诗三百首启动啦！！！");
+    }
+}
